d16755747c99cdd524fbf41c6876d6aeaee6377c
RahulDas-dev <r.das699@gmail.com>
2024-01-15 22:14:33 +0530

    Added hyter Paamter code

---
+++ tune_sklearn/__init__.py
@@ -0,0 +1,5 @@
+from .tune_gridsearch import TuneGridSearchCV
+from .tune_search import TuneSearchCV
+from ._version import __version__
+
+__all__ = ["TuneGridSearchCV", "TuneSearchCV", "__version__"]
